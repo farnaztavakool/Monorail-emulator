5d9b04c6a7091a720d44e28b1cf908de38ede6d7
feddrick38 <z5238236@ad.unsw.edu.au>
2020-08-06 11:18:15 +1000

    Keypad now can read in three stages: station name, travel time, dwell time. TODO: Put all the input into an array.

--- project/keypad.asm
+++ project/keypad.asm
@@ -15,8 +15,21 @@
 .equ			ROWMASK_INIT =	0b00000001
 .equ			COLMASK_INIT =	0b11101111
 .equ			ROWMASK	=	0b00001111							; mask to check whether one of the key is pressed in a column
-.equ			KEYPAD_NOT_PRESSED = 5								; constant that describes the keypad hasn't been pressed yet
-.equ			BF_POSITION = 7									; constant that contains the busy flag bit position 
+
+.equ			KEYPAD_NOT_PRESSED = 10								; constant that describes the keypad hasn't been pressed yet
+.equ			ASTERISK_PRESSED = 11 
+.equ			ENTER_PRESSED = 12								; constant indicating ENTER key has been pressed
+.equ			OTHER_CHARACTER_PRESSED	 = 0							; constant returned by display_pressed's function to indicates character other than
+													; ENTER has been pressed
+
+.equ			STATION_NAME_READING = 0x1
+.equ			TRAVEL_TIME_READING = 0x2
+.equ			DWELL_TIME_READING = 0x4							
+.equ			FINISH_READING = 0x8								; these constants indicate which stages the input reading is currently in 
+
+.equ			STATION_NAME_READING_BIT = 0
+.equ			TRAVEL_TIME_READING_BIT = 1		
+.equ			DWELL_TIME_READING_BIT = 3								; these constants correspond to the bit position of the above constants
 
 .equ			delay1ms = 1776									; constant for wait_1ms
 
@@ -24,11 +37,12 @@
 .equ			LCD_E = 6
 .equ			LCD_RW = 5
 .equ			LCD_BE = 4									; constant for LCD
+.equ			BF_POSITION = 7									; constant that contains the busy flag bit position 
 
 .equ			waste_n = $FFFF									; constant for switch_delay
 
-.equ			four_hundred_ms_num_interrupts = 977						; constant indicating how many time timer0 ovf 
-													; should occur before 200ms has passed
+.equ			one_second_num_interrupts = 977							; constant indicating how many time timer0 ovf 
+													; should occur before 1s has passed
 
 .def			zero = r2
 .def			temp1 = r16
@@ -42,6 +56,9 @@
 .def			mask1 = r22
 .def			row = r3
 .def			col = r4									; registers used for keypad
+
+.def			return_val_l = r24
+.def			return_val_h = r25								; return__val_h:return_val_l as a return register
 
 
 
@@ -161,11 +178,34 @@ if_no_shift_lcd_left:
 	cbi		PORTA, @0
 .endmacro
 
+; macro that checks whether row == 0 && col == 0. If yes, @0 = 1 else @0 = 0
+; Note: this macro assumes @0 to be r16-r31
+.macro		check_row_col_zero
+	ldi		@0, 1								; @0 = 1 (initially assumes row = 0 & col = 0)
+check_row_zero:
+	cp		row, zero
+	breq		check_col_zero							; if row == 0, goto check_col_zero
+	ldi		@0, 0								; else @0 = 0, and return from macro
+	rjmp		check_row_col_zero_end
+check_col_zero:
+	cp		col, zero							 
+	breq		check_row_col_zero_end						; if col == 0, return from macro
+	ldi		@0, 0								; else @0 = 0, and return from macro
+check_row_col_zero_end:
+.endmacro
 ; macro to find the character corresponding to the key enterred in the keypad and the return value stored in @0
-; Note: this function assumes @1 to be register between r16-r31
+; Note: if (row, col) = (0, 0), this macro returns 'Y'
+; Note: this function assumes @1 to be register between r16-r31.
 ; e.g.	1. if (row, col) = (0, 1), this macro returns 'A' in @0
 ;	2. if (row, col) = (2, 1), this macro returns 'P' in @0
 .macro		find_character_according_row_col
+	cp			row, zero
+	brne			if_not_row_col_zero
+	cp			col, zero
+	brne			if_not_row_col_zero
+	ldi			@0, 'Y'
+	rjmp			find_character_according_row_col_end
+if_not_row_col_zero:
 	mov			@0, row							; @0 = row
 	ldi			@1, 9							; @1 = 9
 	mul			@0, @1							; r1:r0 = 9 * row					
@@ -180,6 +220,26 @@ if_no_shift_lcd_left:
 	subi			@1, 3							; @1 = 'A' - 3
 
 	add			@0, @1							; @0 = ('A' - 3) +  (9 * row + 3 * col)
+find_character_according_row_col_end:
+.endmacro
+
+; macro that start timer0 if input_reading_stage == STATION_NAME_READING, else stops the timer 0
+.macro set_timer0_according_to_input_stage
+	lds		macro_r1, input_reading_stage
+	cpi		macro_r1, STATION_NAME_READING
+	brne		stop_timer0
+	rcall		timer0_initialization
+	rjmp		set_timer0_according_to_input_stage_end
+stop_timer0:
+	rcall		timer0_stop
+set_timer0_according_to_input_stage_end:
+.endmacro
+
+; macro to convert a SINGLE digit character in @0 to ascii
+; and store the result into @0
+.macro		convert_digit_to_ascii
+	ldi		macro_r1, '0'
+	add		@0, macro_r1
 .endmacro
 
 ; macro that do @1 % @0, and store the value onto @1. Note: @0 is expected to be a constant
@@ -210,6 +270,10 @@ prev_col:				.byte	1					; prev row and col will store the previous key pressed 
 last_typed_character:			.byte	1					; a variable to store the last typed character on the LCD 
 
 num_timer0_ovf:				.byte	2					; a variable to store how many time timer0 has overflowed
+
+input_reading_stage:			.byte	1					; a variable to keep track in which stage of input reading are the program currently in
+
+cur_num_parameters:			.byte	1					; a variable to keep track how many parameters we currently have 
 
 .cseg	
 		
@@ -262,10 +326,13 @@ keypad_initialization:
 	;ser			temp1
 	;out			PORTF, temp1						;  enable R0-R3 pull up resistor. Open all C0-C3 switch
 
+	ldi			temp1, STATION_NAME_READING
+	sts			input_reading_stage, temp1				; input_reading_stage = STATION_NAME_READING
+
 	rcall			timer0_initialization					; turn on timer0 with pre-scaler of 64
 
 	sei										; enable global interrupt (mainly for timer0OVF currently)
-main:
+main_station_name:
 	clr			col
 	ldi			mask1, COLMASK_INIT					; mask = 0b11101111
 col_loop:
@@ -285,19 +352,41 @@ row_loop:
 	brne			continue_scanning_row					; if temp2 & mask != 0, goto continue_scanning_row
 											; else we found the key pressed,
 
+	
+	/* ///////////////////////////////////////////////// WORKING ///////////////////////////////////////////////////////// */
+	lds			temp1, input_reading_stage
+
+	cpi			temp1, STATION_NAME_READING
+	brne			display_time_character					; if input_reading_stage != STATION_TIME_READING, goto display_time_character
+											; else, display_station_name_character
+display_station_name_character:
 	cli										; clear global interrupt bit
-	rcall			display_pressed_character				; display the pressed character onto LCD
+
+	rcall			display_pressed_character_station_name			; display the pressed character onto LCD
 
 	in			temp1, TIFR0
 	cbr			temp1, 0x01
 	out			TIFR0, temp1						; clear the timer0 ovf interrupt flag (not having this is okay, since it only affects
 											; 1 counter increment in num_timer0_ovf)
 
-	sei										; NOTE: it is important to make sure display_pressed_character won't be interrupted
+	sei										; NOTE: it is important to make sure display_pressed_character_station_name won't be interrupted
 											; by timer0 overflow
-
+	
+	rjmp			display_pressed_character_finished
+display_time_character:
+	rcall			display_pressed_digit_time			
+	rjmp			display_pressed_character_finished
+display_pressed_character_finished:
 	rcall			switch_delay						; delay for approx 0.124sec
-	rjmp			main							; continue scanning new pressed key
+	cpi			return_val_l, ENTER_PRESSED
+	breq			main_station_name_end					; if the "ENTER" key is pressed, break out the current stage input reading and
+											; move on to reading the next sgae
+
+	cpi			return_val_l, ASTERISK_PRESSED
+	breq			input_reading_finish					; if the "ASTERISK" key is pressed, break out of the entire reading stage
+											; and move on to the next stage of simulating the railwy
+
+	rjmp			main_station_name					; continue scanning new pressed key
 
 continue_scanning_row:
 	inc			row							; row++
@@ -310,22 +399,78 @@ no_switch_pressed:
 	inc			col							; col++
 	ldi			temp3, 4						; temp3 = 4
 	cp			col, temp3
-	breq			relative_branch_resolve_main				; if col == 4, goto main
-	rjmp			relative_branch_resolve_main_not_needed
-relative_branch_resolve_main:
-	rjmp			main
-relative_branch_resolve_main_not_needed:
+	breq			relative_branch_resolve_main_station_name		; if col == 4, goto main
+	rjmp			relative_branch_resolve_main_station_namenot_needed
+relative_branch_resolve_main_station_name:
+	rjmp			main_station_name
+relative_branch_resolve_main_station_namenot_needed:
 	rjmp			col_loop						; else, goto col_loop
+main_station_name_end:
+	clear_lcd_display								; clear the display to read the next input	
+	rcall			change_input_reading_stage				; call change_input_reading_stage to change input_reading_stage variable accordingly
+	cli
+	set_timer0_according_to_input_stage						; macro that stops timer0 if input reading stage != STATION_NAME_READING, else it initialize it
+	sei										; NOTE: this macro shouldn't be interrupted by timer0 (since it is changing the state of it)
+	rjmp			main_station_name
+input_reading_finish:
+	clear_lcd_display
+	rjmp			input_reading_finish					; Input reading is finished at this stage
 
 
+; A function that display the pressed number in the keypad to the screen. This function return ENTER_PRESSED if the 'A' key is pressed on the keypad,
+; otherwise, it returns either the number 0-9.
+; Registers: temp1
+; Arguments: row, col
+; Returns: return_val_l (either contain 0-9 or ENTER_PRESSED)
+display_pressed_digit_time:
+	push			temp1
 
-; A function that display the pressed character in the keypad to the screen. Note: This function need to synchronize with timer0
+	ldi			temp1, 3							; temp1 = 3
+	cp			row, temp1
+	breq			check_if_zero_character						; if row == 3, goto check_if_zero_character_pressed
+
+	cp			col, temp1
+	breq			check_if_enter_pressed_time					; if col == 3, goto check_if_enter_pressed_time
+
+	mov			temp1, row							; temp1 = row
+	lsl			temp1								; temp1 = 2 * row
+	add			temp1, row							; temp1 = 3 * row
+	add			temp1, col							; temp1 = 3 * row + col
+	inc			temp1								; temp1 = 3 * row + (col + 1)
+	mov			return_val_l, temp1						; return value = 0 ... 9
+	convert_digit_to_ascii	temp1
+	do_display_a_character	temp1								; display the digit into lcd display
+	rjmp			display_pressed_digit_time_end					; return
+
+check_if_enter_pressed_time:
+	ldi			temp1, 0							; temp1 = 0
+	cp			row, temp1
+	brne			display_pressed_digit_time_end					; if row != 0, goto display_pressed_digit_time_end
+	ldi			return_val_l, ENTER_PRESSED					; else, set return_val_l = ENTER_PRESSED
+	rjmp			display_pressed_digit_time_end					; return
+check_if_zero_character:
+	ldi			temp1, 1							; temp1 = 1
+	cp			col, temp1
+	brne			display_pressed_digit_time_end					; if col != 1, it is not valid character, so we just ignore it
+	ldi			temp1, '0'							; display '0'
+	do_display_a_character	temp1	
+	ldi			return_val_l, 0; set return value = 0
+display_pressed_digit_time_end:
+	pop			temp1
+	ret
+
+; A function that display the pressed character in the keypad to the screen. This function return ENTER_PRESSED if the 'A' key is pressed on the keypad, ASTERISK_PRESSED
+; if the '*' key is pressed, otherwise, it returns OTHER_CHARACTER_PRESSED
+; Note: This function need to synchronize with timer0
 ; Registers: temp1, temp2
-; Arguments: prev_row, prev_col, last_typed_character
-; Returns: -
-display_pressed_character:
+; Arguments: prev_row, prev_col, last_typed_character, row, col
+; Returns: return_val_l (either contain OTHER_CHARACTER_PRESSED, ASTERISK_PRESSED or ENTER_PRESSED)
+display_pressed_character_station_name:
 	push			temp1
 	push			temp2
+	push			temp3
+
+	ldi			return_val_l, OTHER_CHARACTER_PRESSED			; initially, just assume OHTER_CHARACTER_PRESSED
 	
 	lds			temp1, prev_row
 	cp			temp1, row
@@ -349,6 +494,12 @@ relative_branch_not_needed2:
 	dec			temp1							; LCD pointer adddress--
 	do_store_lcd_pointer_address temp1						; update the DD-RAM address counter in lcd
 
+	lds			temp1, current_lcd_pointer_pos				; temp1 = current_lcd_pointer_pos
+	cpi			temp1, MAXCHAR_DISPLAY + 1			
+	brlt			if_no_shift_lcd_right					; if current_lcd_pointer_pos < current_lcd_pointer_pos
+											; goto if_no_shift_lcd_right
+	shift_lcd_right									; else, shift the lcd to the right.
+if_no_shift_lcd_right:
 	lds			temp1, current_lcd_pointer_pos
 	dec			temp1
 	sts			current_lcd_pointer_pos, temp1				; pre-decrement current_lcd_pointer_pos, since we are only overwriting the previous
@@ -360,38 +511,38 @@ relative_branch_not_needed2:
 
 	sub			temp2, temp1						; temp2 = last_typed_character - the first character in the key
 	inc			temp2							; temp2++
+	check_row_col_zero	temp3							; temp1 = 1 if row & col is 0, otherwise temp1 = 0
+	cpi			temp3, 1
+	brne			if_row_col_not_zero
+	modulus_one_byte_by	2, temp2						; temp2 %= 2
+	rjmp			done_modulus_character					; goto done_modulus_character
+if_row_col_not_zero:
 	modulus_one_byte_by	3, temp2						; temp2 %= 3
+done_modulus_character:
 
 	add			temp2, temp1						; temp2 +=  the first character in the key
 
 	do_display_a_character	temp2
 	 
 	sts			last_typed_character, temp2				; last_typed_character = temp2
-	sts			num_timer0_ovf, zero
-	sts			num_timer0_ovf + 1, zero
-	rjmp			display_pressed_character_end
+	clear_two_bytes		num_timer0_ovf
+
+	ldi			return_val_l, OTHER_CHARACTER_PRESSED			; returl_l = OTHER_CHARACTER_PRESSED
+	rjmp			display_pressed_character_station_name_end
 
 
 new_character_pressed:
-	mov			temp1, row
-	cpi			temp1, 3
-	breq			display_pressed_character_end				; if row == 3, goto display_pressed_character_end
+	ldi			temp1, 3						; temp1 = 3
+	cp			col, temp1
+	breq			check_third_column1					; if col == 3, goto check_third_column1
 
-	mov			temp1, col
-	cpi			temp1, 3
-	breq			display_pressed_character_end				; if col == 3, goto display_pressed_character_end
-check_one_is_pressed:
-	cpi			temp1, 0
-	brne			search_for_new_character_to_display			; if col != 0, goto search_for_new_character_to_display
+	cp			row, temp1
+	breq			check_third_row1					; if row == 3, goto check_third_row1
+											; else, row and col is not 3, find the new chararacter to display
 
-	mov			temp1, row
-	cpi			temp1, 0
-	brne			search_for_new_character_to_display			; if row != 0, goto search_for_new_character_to_display
-
-	rjmp			display_pressed_character_end				; else, row == 0 && col == 0, we ignore the key "1"
 search_for_new_character_to_display:
 	find_character_according_row_col	temp1, temp2				; temp1 will contain the first character corresponding to which key pressed
-											; e.g temp1 can contain A, D, G, J, M, P, T, W
+											; e.g temp1 can contain A, D, G, J, M, P, S, V, Y
 
 	do_display_a_character	temp1							; display the new character onto the lcd
 		
@@ -399,11 +550,47 @@ search_for_new_character_to_display:
 	sts			prev_row, row						; prev_row = row
 	sts			prev_col, col						; prev_col = col
 
-	sts			num_timer0_ovf, zero
-	sts			num_timer0_ovf + 1, zero
+	clear_two_bytes		num_timer0_ovf
 
-display_pressed_character_end:
+	ldi			return_val_l, OTHER_CHARACTER_PRESSED			; return_val_; = OTHER_CHARACTER_PRESSED
+	rjmp			display_pressed_character_station_name_end
 
+check_third_column1:
+	ldi			temp1, 0
+	cp			row, temp1
+	brne			check_third_column1_end
+	ldi			return_val_l, ENTER_PRESSED				; return_l = ENTER_PRESSED
+check_third_column1_end:
+	rjmp			display_pressed_character_station_name_end	
+
+check_third_row1:
+	ldi			temp1, 0						; temp1 = 0
+	cp			col, temp1
+	brne			check_space_key_pressed					; if col != 0, goto check_space_key_pressed
+											; otherwise, we know that the ASTERISK key has been pressed
+	ldi			return_val_l, ASTERISK_PRESSED				; return_val_l = ASTERISK_PRESSED
+	rjmp			check_third_row1_end
+
+check_space_key_pressed:
+	ldi			temp1, 1
+	cp			col, temp1
+	brne			check_third_row1_end
+space_key_pressed:
+	ldi			temp1, ' '						; temp1 = ' '
+	do_display_a_character	temp1							
+
+	ldi			temp1, KEYPAD_NOT_PRESSED
+	sts			last_typed_character, temp1				
+	sts			prev_row, temp1						
+	sts			prev_col, temp1						; last_typed_character, prev_row, prev_col = KEYPAD_NOT_PRESSED
+	
+	clear_two_bytes		num_timer0_ovf						; reset the timer0 count
+		
+check_third_row1_end:
+	rjmp			display_pressed_character_station_name_end
+display_pressed_character_station_name_end:
+
+	pop			temp3
 	pop			temp2
 	pop			temp1
 	ret
@@ -426,12 +613,12 @@ Timer0OVF:
 	add			temp1, temp3				
 	adc			temp2, zero						; temp2:temp1 =  num_timer0_ovf + 1
 	
-	ldi			temp3, low(four_hundred_ms_num_interrupts)
-	ldi			temp4, high(four_hundred_ms_num_interrupts)		; temp4:temp3 = four_hundred_ms_num_interrupts
+	ldi			temp3, low(one_second_num_interrupts)
+	ldi			temp4, high(one_second_num_interrupts)			; temp4:temp3 = one_second_num_interrupts
 
 	cp			temp1, temp3
 	cpc			temp2, temp4
-	brlt			Timer0OVF_end						; if temp2:temp1 < four_hundred_ms_num_interrupts, goto Timer0OVF_end
+	brlt			Timer0OVF_end						; if temp2:temp1 < one_second_num_interrupts, goto Timer0OVF_end
 											; else 400ms has passed, reset prev_col, prev_row, last_typed_character 
 	ldi			temp1, KEYPAD_NOT_PRESSED
 	sts			prev_col, temp1
@@ -492,7 +679,36 @@ timer0_stop:
 	pop			temp2
 	pop			temp1
 	ret		
-	
+
+; A function that changes input_reading_stage variable from one stage to the next stage, showcased below.
+; STATION_NAME_READING ->  TRAVEL_TIME_READING -> DWELL_TIME_READING -> STATION_NAME_READING -> ...
+; Registers: temp1
+; Arguments: input_reading_stage
+; Returns: - (only changes input_reading_stage value)
+change_input_reading_stage:
+	push			temp1
+	lds			temp1, input_reading_stage				; temp1 = input_reading_stage
+
+	cpi			temp1, STATION_NAME_READING
+	brne			check_if_travel_time_reading				; if input_reading_stage != STATION_NAME_READING, goto check_if_travel_time_reading
+	ldi			temp1, TRAVEL_TIME_READING				
+	sts			input_reading_stage, temp1				; else, input_reading_stage = TRAVEL_TIME_READING
+	rjmp			change_input_reading_stage_end				; return
+
+check_if_travel_time_reading:
+	cpi			temp1, TRAVEL_TIME_READING				
+	brne			check_if_dwell_time_reading				;  if input_reading_stage != TRAVEL_TIME_READING, goto check_if_dwell_time_reading
+	ldi			temp1, DWELL_TIME_READING
+	sts			input_reading_stage, temp1				; else, input_reading_stage, = DWELL_TIME_READING
+	rjmp			change_input_reading_stage_end				; return
+
+check_if_dwell_time_reading:
+	; If we arrive at this stage, input_reading_stage should be DWELL_TIME_READING (no need to check)
+	ldi			temp1, STATION_NAME_READING
+	sts			input_reading_stage, temp1				; input_readign_stage = STATION_NAME_READING
+change_input_reading_stage_end:
+	pop			temp1
+	ret
 
 ; A function that set temp as 0xFF and decrements it until 0
 ; Registers: temp1
